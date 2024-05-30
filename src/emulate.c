fb8612be0110b0f4f2e3c1c5ccdfd8f1f3a82221
Lihan Yang <yanglihancyrus@gmail.com>
2024-05-30 18:09:21 +0100

    Added registers in emulate.c

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
 
+union reg {
+  int64_t x;
+  int32_t w;
+} r[31], zr, pc, sp;
+
+enum {
+  N = -1, Z = 0, C = 1, V = 2
+} pstate = Z;
+
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
 }
