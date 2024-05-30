e2d43a532506c6169672d4fa9b94626170fd942c
Lihan Yang <yanglihancyrus@gmail.com>
2024-05-30 18:16:04 +0100

    Added Branches

--- src/emulate.c
+++ src/emulate.c
@@ -3,12 +3,21 @@
 union reg {
   int64_t x;
   int32_t w;
-} r[31], zr, pc, sp;
+} r[31], zr = {0}, pc, sp;
+
+int64_t x(int id) {
+  return r[id].x;
+}
+
+int32_t w(int id) {
+  return r[id].w;
+}
 
 enum {
   N = -1, Z = 0, C = 1, V = 2
 } pstate = Z;
 
 int main(int argc, char **argv) {
+  
   return EXIT_SUCCESS;
 }
