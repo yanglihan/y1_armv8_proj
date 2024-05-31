6d558bbdf8b632c8d18c620bba8c701d4c43fba3
Lihan Yang <yanglihancyrus@gmail.com>
2024-05-31 13:49:13 +0100

    fixed minor bugs

--- src/emulate.c
+++ src/emulate.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef u_int32_t instr;
-typedef u_int32_t seg; // using seg for a segment of instructions
+typedef uint32_t instr;
+typedef uint32_t seg; // using seg for a segment of instructions
 union reg
 {
   int64_t x;
   int32_t w;
-  u_int64_t ux;
-  u_int32_t uw;
-} r[31], zr, pc, sp; // registers
+  uint64_t ux;
+  uint32_t uw;
+} r[32], *zr = r + 31, pc, sp; // registers
 
 struct
 {
@@ -22,28 +22,28 @@ int main(int argc, char **argv)
   return EXIT_SUCCESS;
 }
 
-u_int64_t takebit(void *from, int begin, int size)
+uint64_t takebits(void *from, int begin, int size)
 {
-  u_int64_t i = *(u_int64_t *)from;
+  uint64_t i = *(uint64_t *)from;
   i >>= begin;
-  i ^= i >> size;
+  i &= (1 << size) - 1;
   return i;
 }
 
 void dpi(instr instr)
 {
-  seg sf = takebit(&instr, 31, 1);
-  seg opc = takebit(&instr, 29, 2);
-  seg opi = takebit(&instr, 26, 3);
-  seg opr = takebit(&instr, 5, 18);
-  seg rdi = takebit(&instr, 0, 5);
+  seg sf = takebits(&instr, 31, 1);
+  seg opc = takebits(&instr, 29, 2);
+  seg opi = takebits(&instr, 26, 3);
+  seg opr = takebits(&instr, 5, 18);
+  seg rdi = takebits(&instr, 0, 5);
   union reg *rd = r + rdi;
-  seg sh = takebit(&instr, 22, 1);
-  seg imm12 = takebit(&opr, 10, 12) << (sh * 12);
-  seg rni = takebit(&opr, 5, 5);
+  seg sh = takebits(&instr, 22, 1);
+  seg imm12 = takebits(&opr, 10, 12) << (sh * 12);
+  seg rni = takebits(&opr, 5, 5);
   union reg *rn = r + rni;
-  seg hw = takebit(&instr, 21, 2);
-  seg imm16 = takebit(&instr, 5, 16) << (hw * 16);
+  seg hw = takebits(&instr, 21, 2);
+  seg imm16 = takebits(&instr, 5, 16) << (hw * 16);
 
   switch (opi)
   {
@@ -65,7 +65,7 @@ void dpi(instr instr)
       if (sh)  // 64-bit
       {
         rd->x = rn->x + (int64_t)imm12;
-        pstate.n = takebit(rd, 63, 1);
+        pstate.n = takebits(rd, 63, 1);
         pstate.z = (rd->x == 0);
         pstate.c = (UINT64_MAX - imm12 > rn->x);
         pstate.v = (INT64_MAX - (int64_t)imm12 > rn->x);
@@ -73,7 +73,7 @@ void dpi(instr instr)
       else // 32-bit
       {
         rd->w = rn->w + (int32_t)imm12;
-        pstate.n = takebit(rd, 31, 1);
+        pstate.n = takebits(rd, 31, 1);
         pstate.z = (rd->w == 0);
         pstate.c = (UINT32_MAX - imm12 > rn->w);
         pstate.v = (INT32_MAX - (int32_t)imm12 > rn->w);
@@ -93,7 +93,7 @@ void dpi(instr instr)
       if (sh)  // 64-bit
       {
         rd->x = rn->x - (int64_t)imm12;
-        pstate.n = takebit(rd, 63, 1);
+        pstate.n = takebits(rd, 63, 1);
         pstate.z = (rd->x == 0);
         pstate.c = (imm12 > rn->x);
         pstate.v = (INT64_MIN + (int64_t)imm12 > rn->x);
@@ -101,7 +101,7 @@ void dpi(instr instr)
       else // 32-bit
       {
         rd->w = rn->w - (int32_t)imm12;
-        pstate.n = takebit(rd, 31, 1);
+        pstate.n = takebits(rd, 31, 1);
         pstate.z = (rd->w == 0);
         pstate.c = (imm12 > rn->w);
         pstate.v = (INT32_MIN + (int32_t)imm12 > rn->w);
@@ -117,33 +117,31 @@ void dpi(instr instr)
     case 0b00: // movn
       if (sf)  // 64-bit
       {
-        rd->ux = !(u_int64_t)imm16;
+        rd->ux = !(uint64_t)imm16;
       }
       else // 32-bit
       {
-        rd->ux = 0;
-        rd->uw = !(u_int32_t)imm16;
+        rd->ux = !(uint32_t)imm16; // ux used purposefully to set upper half to zero
       }
       break;
     case 0b10: // movz
       if (sf)  // 64-bit
       {
-        rd->ux = (u_int64_t)imm16;
+        rd->ux = (uint64_t)imm16;
       }
       else // 32-bit
       {
-        rd->ux = 0;
-        rd->uw = (u_int32_t)imm16;
+        rd->ux = (uint32_t)imm16; // ux used purposefully to set upper half to zero
       }
       break;
     case 0b11: // movk
       if (sf)  // 64-bit
       {
-        rd->ux = (u_int64_t)imm16 | (rd->ux & !((u_int64_t)0xffff << (hw * 16)));
+        rd->ux = (uint64_t)imm16 | (rd->ux & !((uint64_t)0xffff << (hw * 16)));
       }
       else // 32-bit
       {
-        rd->uw = (u_int32_t)imm16 | (rd->uw & !((u_int32_t)0xffff << (hw * 16)));
+        rd->ux = (uint32_t)imm16 | (rd->uw & !((uint32_t)0xffff << (hw * 16)));  // ux used purposefully to set upper half to zero
       }
       break;
     default:
