9c170017197f3b13ffdb8e0d1c76512e1296c593
Lihan Yang <yanglihancyrus@gmail.com>
2024-06-06 11:44:42 +0100

    Updated comments of primary functions, added typedefs reg_t, seg_t, instr_t, addr_t

--- src/emulate.c
+++ src/emulate.c
@@ -1,29 +1,22 @@
+
+#include "emulate.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MEM_SIZE 0x200000 // 2MB of memory
 uint8_t mem[MEM_SIZE];
 
-union reg
-{
-  int64_t x;
-  int32_t w;
-  uint64_t ux;
-  uint32_t uw;
-  uint32_t upper[2];
-} r[32], *zr = r + 31, pc, sp;
-
-typedef uint32_t instr;
-typedef uint32_t seg;
+reg_t r[32], *zr = r + 31, pc, sp;
 
 struct
 {
   bool n, z, c, v;
 } pstate = {0, 1, 0, 0};
 
-uint64_t mem64_load(uint64_t addr)
+uint64_t mem64_load(addr_t addr)
 {
   if (addr + 7 >= MEM_SIZE)
   {
@@ -37,7 +30,7 @@ uint64_t mem64_load(uint64_t addr)
   return value;
 }
 
-void mem64_store(uint64_t addr, uint64_t data)
+void mem64_store(addr_t addr, uint64_t data)
 {
   if (addr + 7 >= MEM_SIZE)
   {
@@ -49,7 +42,7 @@ void mem64_store(uint64_t addr, uint64_t data)
   }
 }
 
-uint32_t mem32_load(uint64_t addr)
+uint32_t mem32_load(addr_t addr)
 {
   if (addr + 3 >= MEM_SIZE)
   {
@@ -63,7 +56,7 @@ uint32_t mem32_load(uint64_t addr)
   return value;
 }
 
-void mem32_store(uint64_t addr, uint32_t data)
+void mem32_store(addr_t addr, uint32_t data)
 {
   if (addr + 3 >= MEM_SIZE)
   {
@@ -75,7 +68,7 @@ void mem32_store(uint64_t addr, uint32_t data)
   }
 }
 
-inline uint64_t take_bits(void *from, int begin, int size)
+uint64_t take_bits(void *from, int begin, int size)
 {
   uint64_t i = *(uint64_t *)from;
   i >>= begin;
@@ -83,7 +76,7 @@ inline uint64_t take_bits(void *from, int begin, int size)
   return i;
 }
 
-uint32_t bit_shift32(seg opr, seg oprand, int shift_amount)
+uint32_t bit_shift32(seg_t opr, seg_t oprand, int shift_amount)
 {
   switch (opr & 0b0110)
   {
@@ -103,7 +96,7 @@ uint32_t bit_shift32(seg opr, seg oprand, int shift_amount)
   return 0;
 }
 
-uint64_t bit_shift64(seg opr, uint64_t oprand, int shift_amount)
+uint64_t bit_shift64(seg_t opr, uint64_t oprand, int shift_amount)
 {
   switch (opr & 0b0110)
   {
@@ -123,20 +116,20 @@ uint64_t bit_shift64(seg opr, uint64_t oprand, int shift_amount)
   return 0;
 }
 
-void dpi(instr instr)
+void dpi(instr_t instr) // data processing (immediate)
 {
-  seg sf = take_bits(&instr, 31, 1);
-  seg opc = take_bits(&instr, 29, 2);
-  seg opi = take_bits(&instr, 26, 3);
-  seg operand = take_bits(&instr, 5, 18);
-  seg rdi = take_bits(&instr, 0, 5);
-  union reg *rd = r + rdi;
-  seg sh = take_bits(&instr, 22, 1);
-  seg imm12 = take_bits(&instr, 10, 12) << (sh * 12);
-  seg rni = take_bits(&instr, 5, 5);
-  union reg *rn = r + rni;
-  seg hw = take_bits(&instr, 21, 2);
-  seg imm16 = take_bits(&instr, 5, 16) << (hw * 16);
+  seg_t sf = take_bits(&instr, 31, 1);
+  seg_t opc = take_bits(&instr, 29, 2);
+  seg_t opi = take_bits(&instr, 23, 3);
+  seg_t operand = take_bits(&instr, 5, 18);
+  seg_t rdi = take_bits(&instr, 0, 5);
+  reg_t *rd = r + rdi;
+  seg_t sh = take_bits(&instr, 22, 1);
+  seg_t imm12 = take_bits(&instr, 10, 12) << (sh * 12);
+  seg_t rni = take_bits(&instr, 5, 5);
+  reg_t *rn = r + rni;
+  seg_t hw = take_bits(&instr, 21, 2);
+  seg_t imm16 = take_bits(&instr, 5, 16) << (hw * 16);
 
   switch (opi)
   {
@@ -190,7 +183,7 @@ void dpi(instr instr)
         rd->x = rn->x - (int64_t)imm12;
         pstate.n = take_bits(rd, 63, 1);
         pstate.z = (rd->ux == 0);
-        pstate.c = (imm12 > rn->ux);
+        pstate.c = (imm12 <= rn->ux);
         pstate.v = (INT64_MIN + (int64_t)imm12 > rn->x);
       }
       else // 32-bit
@@ -199,7 +192,7 @@ void dpi(instr instr)
         rd->w = rn->w - (int32_t)imm12;
         pstate.n = take_bits(rd, 31, 1);
         pstate.z = (rd->uw == 0);
-        pstate.c = (imm12 > rn->uw);
+        pstate.c = (imm12 <= rn->uw);
         pstate.v = (INT32_MIN + (int32_t)imm12 > rn->w);
       }
       break;
@@ -211,12 +204,12 @@ void dpi(instr instr)
     case 0b00: // movn
       if (sf)  // 64-bit
       {
-        rd->ux = !(uint64_t)imm16;
+        rd->ux = ~(uint64_t)imm16;
       }
       else // 32-bit
       {
         rd->upper[1] = 0;
-        rd->uw = !(uint32_t)imm16;
+        rd->uw = ~(uint32_t)imm16;
       }
       break;
     case 0b10: // movz
@@ -233,12 +226,12 @@ void dpi(instr instr)
     case 0b11: // movk
       if (sf)  // 64-bit
       {
-        rd->ux = (uint64_t)imm16 | (rd->ux & !((uint64_t)0xffff << (hw * 16)));
+        rd->ux = (uint64_t)imm16 | (rd->ux & ~((uint64_t)0xffff << (hw * 16)));
       }
       else // 32-bit
       {
         rd->upper[1] = 0;
-        rd->uw = (uint32_t)imm16 | (rd->uw & !((uint32_t)0xffff << (hw * 16)));
+        rd->uw = (uint32_t)imm16 | (rd->uw & ~((uint32_t)0xffff << (hw * 16)));
       }
       break;
     }
@@ -247,21 +240,21 @@ void dpi(instr instr)
   zr->uw = 0;
 }
 
-void dpr(instr instr)
+void dpr(instr_t instr) // data processing (register)
 {
-  seg sf = take_bits(&instr, 31, 1);
-  seg opc = take_bits(&instr, 29, 2);
-  seg m = take_bits(&instr, 28, 1);
-  seg opr = take_bits(&instr, 21, 4);
-  seg rmi = take_bits(&instr, 16, 5);
-  seg operand = take_bits(&instr, 10, 6);
-  seg rni = take_bits(&instr, 5, 5);
-  seg rdi = take_bits(&instr, 0, 4);
-  seg rai = take_bits(&instr, 10, 5);
-  union reg *rm = r + rmi;
-  union reg *rn = r + rni;
-  union reg *rd = r + rdi;
-  union reg *ra = r + rai;
+  seg_t sf = take_bits(&instr, 31, 1);
+  seg_t opc = take_bits(&instr, 29, 2);
+  seg_t m = take_bits(&instr, 28, 1);
+  seg_t opr = take_bits(&instr, 21, 4);
+  seg_t rmi = take_bits(&instr, 16, 5);
+  seg_t operand = take_bits(&instr, 10, 6);
+  seg_t rni = take_bits(&instr, 5, 5);
+  seg_t rdi = take_bits(&instr, 0, 4);
+  seg_t rai = take_bits(&instr, 10, 5);
+  reg_t *rm = r + rmi;
+  reg_t *rn = r + rni;
+  reg_t *rd = r + rdi;
+  reg_t *ra = r + rai;
   int64_t imm;
 
   if (m)
@@ -355,7 +348,7 @@ void dpr(instr instr)
           rd->x = rn->x - (int64_t)imm;
           pstate.n = take_bits(rd, 63, 1);
           pstate.z = (rd->ux == 0);
-          pstate.c = (imm > rn->ux);
+          pstate.c = (imm <= rn->ux);
           pstate.v = (INT64_MIN + (int64_t)imm > rn->x);
         }
         else // 32-bit
@@ -364,7 +357,7 @@ void dpr(instr instr)
           rd->w = rn->w - (int32_t)imm;
           pstate.n = take_bits(rd, 31, 1);
           pstate.z = (rd->uw == 0);
-          pstate.c = (imm > rn->uw);
+          pstate.c = (imm <= rn->uw);
           pstate.v = (INT32_MIN + (int32_t)imm > rn->w);
         }
         break;
@@ -454,19 +447,19 @@ void dpr(instr instr)
   }
 }
 
-void ls(instr instr)
+void ls(instr_t instr) // loads and stores
 {
-  seg rti = take_bits(&instr, 0, 4);
-  seg xni = take_bits(&instr, 5, 5);
-  seg sf = take_bits(&instr, 30, 1);
-  seg load = take_bits(&instr, 22, 1);
-  seg addr;
-  union reg *rt = r + rti;
-  union reg *xn = r + xni;
+  seg_t rti = take_bits(&instr, 0, 4);
+  seg_t xni = take_bits(&instr, 5, 5);
+  seg_t sf = take_bits(&instr, 30, 1);
+  seg_t load = take_bits(&instr, 22, 1);
+  seg_t addr;
+  reg_t *rt = r + rti;
+  reg_t *xn = r + xni;
 
   if (!take_bits(&instr, 31, 1)) // load literal
   {
-    seg simm19 = take_bits(&instr, 5, 19);
+    seg_t simm19 = take_bits(&instr, 5, 19);
     addr = pc.x + simm19 * 4;
     load = 1;
   }
@@ -474,7 +467,7 @@ void ls(instr instr)
   {
     if (take_bits(&instr, 24, 1)) // unsigned
     {
-      seg imm12 = take_bits(&instr, 10, 12);
+      seg_t imm12 = take_bits(&instr, 10, 12);
       if (sf) // 64-bit
       {
         addr = xn->ux + imm12 * 8;
@@ -486,7 +479,7 @@ void ls(instr instr)
     }
     else if (take_bits(&instr, 21, 1)) // register offset
     {
-      seg xm = take_bits(&instr, 16, 5);
+      seg_t xm = take_bits(&instr, 16, 5);
       if (sf) // 64-bit
       {
         addr = xn->ux + xm;
@@ -498,8 +491,8 @@ void ls(instr instr)
     }
     else // pre/post-index
     {
-      seg simm9 = take_bits(&instr, 12, 9);
-      seg i = take_bits(&instr, 11, 1);
+      seg_t simm9 = take_bits(&instr, 12, 9);
+      seg_t i = take_bits(&instr, 11, 1);
       if (sf) // 64-bit
       {
         addr = xn->ux + i * simm9;
@@ -537,12 +530,12 @@ void ls(instr instr)
   }
 }
 
-void br(instr instr)
+void br(instr_t instr) // branches
 {
-  seg simm26 = take_bits(&instr, 0, 26);
-  seg simm19 = take_bits(&instr, 5, 19);
+  seg_t simm26 = take_bits(&instr, 0, 26);
+  seg_t simm19 = take_bits(&instr, 5, 19);
   int64_t offset;
-  seg cond;
+  seg_t cond;
   int8_t exec;
   switch (take_bits(&instr, 30, 2))
   {
@@ -587,7 +580,7 @@ void br(instr instr)
 
   case 0b11: // register
   {
-    seg xn = take_bits(&instr, 5, 5);
+    seg_t xn = take_bits(&instr, 5, 5);
     pc = r[xn];
   }
   }
@@ -595,65 +588,85 @@ void br(instr instr)
 
 int main(int argc, char **argv)
 {
-  char *input_filename, *output_filename;
-  if (argc == 1) // terminal output
+  printf("argc: %d\n", argc);
+  if (argc == 2) // terminal output
   {
-    input_filename = argv[0];
+    // input_filename = malloc(strlen(argv[1]));
+    printf("Input file: %s\n", argv[1]);
+    // strcpy(input_filename, argv[1]);
   }
-  else if (argc == 2) // file output
+  else if (argc == 3) // file output
   {
-    input_filename = argv[0];
-    output_filename = argv[1];
+    // input_filename = malloc(strlen(argv[1]));
+    // output_filename = malloc(strlen(argv[2]));
+    printf("Input file: %s\n", argv[1]);
+    printf("Output file: %s\n", argv[2]);
+    // strcpy(input_filename, argv[1]);
+    // strcpy(output_filename, argv[2]);
   }
   else
   {
     printf("Invalid number of arguments!\nExpected: emulate input_file [output_file]\n");
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
   }
 
-  FILE *input_file = fopen(input_filename, "rb");
-  instr *buffer;
-  seg op0;
+  FILE *input_file = fopen(argv[1], "rb");
 
-  if (input_file == NULL)
+  if (input_file == NULL) // invalid source file
   {
     printf("Invalid input file!\n");
+    return EXIT_FAILURE;
   }
 
-  while (!feof(input_file))
+  fread(mem, MEM_SIZE, 1, input_file);
+  fclose(input_file);
+
+  while (1)
   {
-    fread(buffer, sizeof(buffer), 1, input_file);
-    op0 = take_bits(buffer, 25, 4);
+    seg_t op0;
+    printf("PC: %08x\n", pc.uw);
+    instr_t instr = mem32_load(pc.ux);
+    op0 = take_bits(&instr, 25, 4);
+    if (instr == 0x8a000000) // halting
+    {
+      printf("-- HLT: %08x\n", instr);
+      break;
+    }
     if ((op0 & 0b1110) == 0b1000) // data processing (immediate)
     {
-      dpi(*buffer);
+      printf("-- DPI: %08x\n", instr);
+      dpi(instr);
     }
     else if ((op0 & 0b0111) == 0b0101) // data processing (register)
     {
-      dpr(*buffer);
+      printf("-- DPR: %08x\n", instr);
+      dpr(instr);
     }
     else if ((op0 & 0b0101) == 0b0100) // loads and stores
     {
-      ls(*buffer);
+      printf("-- LS : %08x\n", instr);
+      ls(instr);
     }
     else if ((op0 & 0b1110) == 0b1010) // branches
     {
-      br(*buffer);
+      printf("-- BR : %08x\n", instr);
+      br(instr);
     }
-    else if (op0 == 0x8a000000) // halting
+    else // unknown command
     {
+      printf("-- UNK: %08x\n", instr);
       break;
     }
+    pc.ux += 4;
   }
-  fclose(input_file);
 
-  if (output_filename)
+  if (argc == 3)
   {
-    FILE *output_file = fopen(output_filename, "w");
+    FILE *output_file = fopen(argv[2], "w");
     if (output_file == NULL)
     {
       printf("Invalid output file!");
-      return EXIT_SUCCESS;
+      return EXIT_FAILURE;
     }
     fprintf(output_file, "Registers:\n");
     for (int i = 0; i < 31; i++)
@@ -663,9 +676,14 @@ int main(int argc, char **argv)
     fprintf(output_file, "PC     = %016llx\n", pc.ux);
     fprintf(output_file, "PSTATE : %c%c%c%c\n", pstate.n ? 'N' : '-', pstate.z ? 'Z' : '-', pstate.c ? 'C' : '-', pstate.v ? 'V' : '-');
     fprintf(output_file, "Non-zero memory:\n");
-    for (uint64_t i = 0; i < MEM_SIZE; i += 8)
+    for (uint64_t i = 0; i < MEM_SIZE; i += 4)
     {
-      fprintf(output_file, "%#010llx: %#010llx\n", i, mem64_load(i));
+      uint64_t m = mem32_load(i);
+      if (!m)
+      {
+        continue;
+      }
+      fprintf(output_file, "%#010llx: %#010llx\n", i, m);
     }
     fclose(output_file);
   }
@@ -679,9 +697,14 @@ int main(int argc, char **argv)
     printf("PC     = %016llx\n", pc.ux);
     printf("PSTATE : %c%c%c%c\n", pstate.n ? 'N' : '-', pstate.z ? 'Z' : '-', pstate.c ? 'C' : '-', pstate.v ? 'V' : '-');
     printf("Non-zero memory:\n");
-    for (int64_t i = 0; i < MEM_SIZE; i += 8)
+    for (int64_t i = 0; i < MEM_SIZE; i += 4)
     {
-      printf("%#010llx: %#010llx\n", i, mem64_load(i));
+      uint64_t m = mem32_load(i);
+      if (!m)
+      {
+        continue;
+      }
+      printf("%#010llx: %#010llx\n", i, m);
     }
   }
 
--- src/emulate.h
+++ src/emulate.h
@@ -6,27 +6,38 @@
 #define MEM_SIZE 0x200000 // 2MB of memory
 uint8_t mem[MEM_SIZE];
 
-union reg;
+union reg
+{
+  int64_t x;
+  int32_t w;
+  uint64_t ux;
+  uint32_t uw;
+  uint32_t upper[2];
+};
 
-typedef uint32_t instr;
-typedef uint32_t seg;
+typedef uint32_t instr_t;
+typedef uint32_t seg_t;
+typedef uint64_t addr_t;
+typedef union reg reg_t;
 
-uint64_t mem64_load(uint64_t addr);
+uint64_t mem64_load(addr_t addr);
 
-void mem64_store(uint64_t addr, uint64_t data);
+void mem64_store(addr_t addr, uint64_t data);
 
-uint32_t mem32_load(uint64_t addr);
+uint32_t mem32_load(addr_t addr);
 
-void mem32_store(uint64_t addr, uint32_t data);
+void mem32_store(addr_t addr, uint32_t data);
 
 inline uint64_t take_bits(void *from, int begin, int size);
 
-uint32_t bit_shift32(seg opr, seg oprand, int shift_amount);
+uint32_t bit_shift32(seg_t opr, seg_t oprand, int shift_amount);
 
-uint64_t bit_shift64(seg opr, uint64_t oprand, int shift_amount);
+uint64_t bit_shift64(seg_t opr, uint64_t oprand, int shift_amount);
 
-void dpi(instr instr);
+void dpi(instr_t instr);
 
-void ls(instr instr);
+void ls(instr_t instr);
 
-void br(instr instr);
+void br(instr_t instr);
+
+void print_state();
